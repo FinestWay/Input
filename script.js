069be558a86f8d9ca5a95111a77ec0fb56b9a474
FinestWay <rrai30430@gmail.com>
2024-02-25 17:36:47 +0530

    added files

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
 let input = document.querySelector('.input-field');
 let img = document.querySelector('.icon');
 let cancel = document.querySelector('.cancel');
+let btn = document.querySelector('.button');
 
 input.addEventListener('click', () => {
     const newClass = input.classList;
     const newImg = img.classList;
     const newCancel = cancel.classList;
+    const newBtn = btn.classList;
 
     newClass.add('input-fields');
     newImg.add('imgs');
     newCancel.add('hide');
+    newBtn.add('submit');
 
     cancel.addEventListener('click', () => {
         newClass.remove('input-fields')
         newImg.remove('imgs')
         newCancel.remove('hide')
+        newBtn.remove('submit');
         input.value = "";
     })
 });
+
+btn.addEventListener('click', () => {
+    
+})
